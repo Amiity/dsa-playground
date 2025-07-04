a2d00a6e2f234121131ded6e477e51388644ef1c
Amiity <oficamit8@gmail.com>
2025-07-04 23:31:42 +0530

    refactor QuickSort

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -1,4 +1,34 @@
 package Sorting;
 
 public class QuickSort {
+
+    public static void main(String[] args) {
+
+        int arr[] = {9,3,1,5,6,2};
+        quickSort(arr, 0, arr.length-1);
+
+    }
+
+    private static void quickSort(int[] arr, int start, int end) {
+
+        sorting(arr, start, pivot);
+
+    }
+
+    private static void sorting(int[] arr, int start, int end) {
+
+       int pivot = arr[end];
+       int i = 0;
+       int j = 0;
+       while(j < end){
+           if(arr[j] > pivot){
+               int temp = arr[j];
+               arr[j] = arr[i];
+               arr[i] = temp;
+               i++;
+           }
+           j++;
+       }
+
+    }
 }
