69c55e74d95641bd236d277b47b526f2f3baf7a1
Amiity <oficamit8@gmail.com>
2025-07-05 23:12:53 +0530

    refactor QuickSort

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -4,31 +4,43 @@ public class QuickSort {
 
     public static void main(String[] args) {
 
-        int arr[] = {9,3,1,5,6,2};
-        quickSort(arr, 0, arr.length-1);
+        int[] arr = {-1, -1, 2, 2, 2, -1};
 
+
+        quickSort(arr, 0, arr.length - 1);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 
     private static void quickSort(int[] arr, int start, int end) {
 
-        sorting(arr, start, pivot);
+        if (start >= end) return;
+
+        int pivot = partition(arr, start, end);
+        quickSort(arr, pivot + 1, end); //right
+        quickSort(arr, start, pivot - 1); //left
 
     }
 
-    private static void sorting(int[] arr, int start, int end) {
+    private static int partition(int[] arr, int start, int end) {
 
-       int pivot = arr[end];
-       int i = 0;
-       int j = 0;
-       while(j < end){
-           if(arr[j] > pivot){
-               int temp = arr[j];
-               arr[j] = arr[i];
-               arr[i] = temp;
-               i++;
-           }
-           j++;
-       }
+        int pivot = arr[end];
+        int i = start;
+        int j = end;
+        while (j < end) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+            j++;
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = pivot;
+        arr[end] = temp;
 
+        return i + 1;
     }
 }
