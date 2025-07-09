122c5c45435af4aa076509903aa47907c94fbb48
Amiity <oficamit8@gmail.com>
2025-07-09 23:54:06 +0530

    leet code 26

--- src/ArrayEasy/RemoveDuplicates.java
+++ src/ArrayEasy/RemoveDuplicates.java
@@ -6,7 +6,7 @@ public class RemoveDuplicates {
         removeDuplicates(arr);
 
         for (int i : arr) {
-            System.out.print(i+ "  ");
+            System.out.print(i + "  ");
         }
     }
 
