1da470ec2e24aaf742b4f3426ab679fb2443aac0
Amiity <oficamit8@gmail.com>
2025-07-20 19:58:16 +0530

    leet code 560

--- src/ArrayEasy/SubarraySubKPositive.java
+++ src/ArrayEasy/SubarraySubKPositive.java
@@ -4,9 +4,11 @@ public class SubarraySubK {
     public static void main(String[] args) {
 
         int[] nums = {10, 5, 2, 7, 1, 9};
+
         int k = 15;
 
         int maxCount = 0;
+        int freq = 0;
         for (int i = 0; i < nums.length; i++) {
             int sum = 0;
             for (int j = i; j < nums.length; j++) {
@@ -15,11 +17,16 @@ public class SubarraySubK {
                     int count = j - i + 1;
                     if (count > maxCount) {
                         maxCount = count;
+                    }else if(count == maxCount){
+                        freq = freq+1;
                     }
                 } else if (sum > k) {
                     break;
                 }
             }
+        }
+        if(freq > maxCount){
+            maxCount = freq+1;
         }
 
         System.out.println(maxCount);
